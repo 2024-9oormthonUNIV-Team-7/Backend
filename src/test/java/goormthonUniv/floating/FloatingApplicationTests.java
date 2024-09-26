744685350af73f698e9286085810cec91bb72e3e
GiHwan2 <sandwich5011@naver.com>
2024-09-26 23:57:42 +0900

    초기 개발 환경 세팅 및 DB연동

---
+++ src/test/java/goormthonUniv/floating/FloatingApplicationTests.java
@@ -0,0 +1,13 @@
+package goormthonUniv.floating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
