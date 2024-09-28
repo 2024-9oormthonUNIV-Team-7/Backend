8e82b36478f6f6b5b712db5fea4d12b87ac32b70
euneun17 <0617ye@gmail.com>
2024-09-29 04:27:27 +0900

    feat: User API 구현

---
+++ src/main/java/goormthonUniv/floating/response/UserResponse.java
@@ -0,0 +1,11 @@
+package goormthonUniv.floating.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+    private String name;
+    private String email;
+}
