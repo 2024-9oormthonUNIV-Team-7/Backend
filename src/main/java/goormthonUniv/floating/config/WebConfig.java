4c1a7abe6d08250ebae4ae60a93b5a18057ae804
GiHwan2 <sandwich5011@naver.com>
2024-09-28 15:50:55 +0900

    feat: CORS 에러 해결

---
+++ src/main/java/goormthonUniv/floating/config/WebConfig.java
@@ -0,0 +1,23 @@
+package goormthonUniv.floating.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // 허용할 도메인 설정
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP 메서드
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
