709416e9bacc15023080427f73ca419acda90c15
GeonHui Lee <spongging@gmail.com>
2024-09-28 14:56:12 +0900

    [feat] 500 error custom

--- src/main/java/goormthonUniv/floating/controller/BalanceGameController.java
+++ src/main/java/goormthonUniv/floating/controller/BalanceGameController.java
@@ -22,9 +22,7 @@ public class BalanceGameController {
     @GetMapping("/api/v1/balance-game")
     public ResponseEntity<BalanceGameResponse> getAllBalanceGames(){
         List<BalanceGame> balanceGames = balanceGameRepository.findAll();
-        if (balanceGames.isEmpty()){
-            throw new SeverErrorException("벨런스 게임 리스트를 불러올 수 없습니다.");
-        }
+
         BalanceGameResponse response = new BalanceGameResponse(balanceGames, 200, "밸런스 게임 전체 조회 성공");
         return ResponseEntity.ok(response);
 
